eaacab41024faa399eb4e36a89316d4350a611bc
Leigh London <leigh.00.london@gmail.com>
2018-07-30 23:07:59 +1000

    factorial w/ tailrec

--- src/main/scala/fpinscala/gettingstarted/Module.scala
+++ src/main/scala/fpinscala/gettingstarted/Module.scala
@@ -2,6 +2,15 @@ package fpinscala.gettingstarted
 
 object Module {
 
+  def factorial(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+    }
+    go(n, 1)
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
 
