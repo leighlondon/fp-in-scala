d999ea2345022dddc54541a62a124dfce2e409ca
Leigh London <leigh.00.london@gmail.com>
2018-07-30 21:29:53 +1000

    initial workings

---
+++ fibonacci/src/main/scala/fpinscala/fibonacci/Fibonacci.scala
@@ -0,0 +1,5 @@
+package fpinscala.fibonacci
+
+object Fibonacci {
+  def apply(n: Int): Int = n
+}
---
+++ fibonacci/src/test/scala/fpinscala/fibonacci/FibonacciSpec.scala
@@ -0,0 +1,14 @@
+package fpinscala.fibonacci
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, Matchers}
+
+@RunWith(classOf[JUnitRunner])
+class FibonacciSpec extends FunSpec with Matchers {
+  describe("Fibonacci") {
+    it("handles the base case") {
+      Fibonacci(1) should be(1)
+    }
+  }
+}
