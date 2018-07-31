fc8efbf4a5219a9bc89d4eaaae86b3b4abaf049d
Leigh London <leigh.00.london@gmail.com>
2018-07-31 21:35:38 +1000

    recursive what

--- src/test/scala/fpinscala/gettingstarted/FibonacciSpec.scala
+++ src/test/scala/fpinscala/gettingstarted/FibonacciSpec.scala
@@ -7,5 +7,17 @@ class FibonacciSpec extends FunSpec with Matchers {
     it("handles the base case") {
       Fibonacci(1) should be(1)
     }
+
+    it("handles the second case") {
+      Fibonacci(2) should be(1)
+    }
+
+    it("handles the third case") {
+      Fibonacci(3) should be(2)
+    }
+
+    it("handles the fourth case") {
+      Fibonacci(4) should be(3)
+    }
   }
 }
