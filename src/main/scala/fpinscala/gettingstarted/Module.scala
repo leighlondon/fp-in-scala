efa2cc563830abad45c30cf0101678ab553dbea8
Leigh London <leigh.00.london@gmail.com>
2018-08-05 17:00:41 +1000

    implementing the uncurrying

--- src/main/scala/fpinscala/gettingstarted/Module.scala
+++ src/main/scala/fpinscala/gettingstarted/Module.scala
@@ -48,5 +48,6 @@ object Module {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     a => b => f(a, b)
 
-  def uncurry[A, B, C](f: A => B => C): (A, B) => C = ???
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
 }
