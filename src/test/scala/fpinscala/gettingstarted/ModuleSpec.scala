46d75b0228ea4fa8459762c8b8486fbc448265f7
Leigh London <leigh.00.london@gmail.com>
2018-08-06 20:13:15 +1000

    expanded tests

--- src/test/scala/fpinscala/gettingstarted/ModuleSpec.scala
+++ src/test/scala/fpinscala/gettingstarted/ModuleSpec.scala
@@ -3,12 +3,25 @@ package fpinscala.gettingstarted
 import fpinscala.gettingstarted.Module._
 import org.scalatest.{FunSpec, Matchers}
 
-class ModuleTest extends FunSpec with Matchers {
+class ModuleSpec extends FunSpec with Matchers {
+
+  def sortedInts(x: Int, y: Int): Boolean = x < y
 
   describe("isSorted") {
     it("handles sorted ints") {
       val as: Array[Int] = Array(1, 2, 3)
       isSorted(as, (x: Int, y: Int) => x <= y) should be(true)
+    }
+
+    it("returns true for a single entry") {
+      val data: Array[Array[Int]] = (0 to 10).map(x => Array(x)).toArray
+      data foreach (oneNumber =>
+        isSorted(oneNumber, sortedInts) should be(true))
+    }
+
+    it("handles negatives properly") {
+      val as: Array[Int] = Array(-1, 0, 1)
+      isSorted(as, sortedInts) should be(true)
     }
   }
 
