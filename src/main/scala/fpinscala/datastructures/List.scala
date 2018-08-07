df7b937c902b1affffa0e2eee960124a26e5d62e
Leigh London <leigh.00.london@gmail.com>
2018-08-07 13:56:41 +1000

    code fixes

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -7,6 +7,16 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 // The 'List' companion object, to provide functions for creating and working
 // with these lists.
 object List {
+
+  // Exercise 3.1, evaluating a match expression
+  val x: Int = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
   def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
@@ -21,17 +31,6 @@ object List {
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
-
-  // Exercise 3.1, evaluating a match expression
-  def aMatchFunction(): Int = {
-    List(1, 2, 3, 4, 5) match {
-      case Cons(x, Cons(2, Cons(4, _))) => x
-      case Nil => 42
-      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-      case Cons(h, t) => h + sum(t)
-      case _ => 101
-    }
-  }
 
   def tail[A](l: List[A]): List[A] = l match {
     case Nil => Nil
--- src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -4,8 +4,8 @@ import org.scalatest.{FunSpec, Matchers}
 
 class ListSpec extends FunSpec with Matchers {
   describe("List") {
-    it("evaluates the random function to 3") {
-      List.aMatchFunction() should be(3)
+    it("evaluates the x match expression to 3") {
+      List.x should be(3)
     }
   }
 }
