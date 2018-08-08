b849c955ba9ef2d4ce78028b04bcda8ee2cb3de9
Leigh London <leigh.00.london@gmail.com>
2018-08-08 21:42:16 +1000

    trying to implement foldRight using foldLeft

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -98,4 +98,7 @@ object List {
 
   def length2[A](l: List[A]): Int =
     foldLeft(l, 0)((z, _) => z + 1)
+
+  // Hard problem - implementing foldRight in terms of foldLeft.
+  def foldRightUsingFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = ???
 }
