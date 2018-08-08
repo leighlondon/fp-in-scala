502cfe413153737f2d75181469914ca60fd3297e
Leigh London <leigh.00.london@gmail.com>
2018-08-08 15:54:03 +1000

    foldRight

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -64,4 +64,9 @@ object List {
     case Cons(h, Nil) => Nil
     case Cons(h, t) => Cons(h, init(t))
   }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
 }
