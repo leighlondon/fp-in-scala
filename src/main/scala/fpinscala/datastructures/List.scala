e1bdf22c9f9a790ea0d3e56e1d3cf732b9c873c7
Leigh London <leigh.00.london@gmail.com>
2018-08-09 13:55:52 +1000

    spec for reverse

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -99,6 +99,8 @@ object List {
   def length2[A](l: List[A]): Int =
     foldLeft(l, 0)((z, _) => z + 1)
 
+  def reverse[A](l: List[A]): List[A] = ???
+
   // Hard problem - implementing foldRight in terms of foldLeft.
   def foldRightUsingFoldLeft[A, B](as: List[A], z: B)(f: (A, B) => B): B = ???
 }
