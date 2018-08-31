d1dea410b238ccb7928679a75e53c67daee80235
Leigh London <leigh.00.london@gmail.com>
2018-08-31 22:46:43 +1000

    flatMap signature

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -155,4 +155,6 @@ object List {
     */
   def filter[A](as: List[A])(f: A => Boolean): List[A] =
     foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = ???
 }
