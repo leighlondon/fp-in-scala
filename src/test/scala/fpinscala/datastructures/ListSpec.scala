ceb2f670f2298c9d0816d786bf07201bc1bf309b
Leigh London <leigh.00.london@gmail.com>
2018-08-31 22:59:09 +1000

    import from the module

--- src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -1,35 +1,36 @@
 package fpinscala.datastructures
 
+import fpinscala.datastructures.List._
 import org.scalatest.{FunSpec, Matchers}
 
 class ListSpec extends FunSpec with Matchers {
   describe("List") {
     it("evaluates the match expression to 3") {
-      List.x_from_match should be(3)
+      x_from_match should be(3)
     }
   }
 
-  describe(".drop") {
+  describe("drop") {
     it("drops the right amount of heads") {
-      List.drop(List(1, 2, 3, 4), 3) should be(List(4))
+      drop(List(1, 2, 3, 4), 3) should be(List(4))
     }
 
     it("remains nil for a nil list") {
-      List.drop(Nil, 1) should be(Nil)
+      drop(Nil, 1) should be(Nil)
     }
 
     it("remains calm for sufficiently large n") {
-      List.drop(List(1), 100000) should be(Nil)
+      drop(List(1), 100000) should be(Nil)
     }
   }
 
-  describe(".init") {
+  describe("init") {
     it("handles .init correctly") {
-      List.init(List(1, 2, 3, 4)) should be(List(1, 2, 3))
+      init(List(1, 2, 3, 4)) should be(List(1, 2, 3))
     }
   }
 
-  describe(".length") {
+  describe("length") {
     it("handles nil input") {
       List.length(Nil: List[Int]) should be(0)
     }
@@ -43,77 +44,77 @@ class ListSpec extends FunSpec with Matchers {
     }
   }
 
-  describe(".reverse") {
+  describe("reverse") {
     it("handles the example usage case") {
-      List.reverse(List(1, 2, 3)) should be(List(3, 2, 1))
+      reverse(List(1, 2, 3)) should be(List(3, 2, 1))
     }
 
     it("handles the string cases") {
-      List.reverse(List("abc", "def", "ghi")) should be(List("ghi", "def", "abc"))
+      reverse(List("abc", "def", "ghi")) should be(List("ghi", "def", "abc"))
     }
   }
 
-  describe(".appendUsingFold") {
+  describe("appendUsingFold") {
     it("appends regular lists correctly") {
-      List.appendUsingFold(List(0, 1), List(2, 3)) should be(List(0, 1, 2, 3))
+      appendUsingFold(List(0, 1), List(2, 3)) should be(List(0, 1, 2, 3))
     }
 
     it("handles two empty lists properly") {
-      List.appendUsingFold(List[Int](), List[Int]()) should be(List[Int]())
+      appendUsingFold(List[Int](), List[Int]()) should be(List[Int]())
     }
 
     it("can take an empty prefix list") {
-      List.appendUsingFold(List[Int](), List(0)) should be(List(0))
+      appendUsingFold(List[Int](), List(0)) should be(List(0))
     }
 
     it("can take an empty suffix list") {
-      List.appendUsingFold(List(100), List[Int]()) should be(List(100))
+      appendUsingFold(List(100), List[Int]()) should be(List(100))
     }
   }
 
-  describe(".concat") {
+  describe("concat") {
     it("works with trivial examples") {
-      List.concat(List(List(0), List(1))) should be(List(0, 1))
+      concat(List(List(0), List(1))) should be(List(0, 1))
     }
   }
 
-  describe(".addOne") {
+  describe("addOne") {
     it("adds basic example") {
-      List.addOne(List(1, 2, 3)) should be(List(2, 3, 4))
+      addOne(List(1, 2, 3)) should be(List(2, 3, 4))
     }
 
     it("handles negative and zero case") {
-      List.addOne(List(-100, -50, 0)) should be(List(-99, -49, 1))
+      addOne(List(-100, -50, 0)) should be(List(-99, -49, 1))
     }
   }
 
-  describe(".doubleToString") {
+  describe("doubleToString") {
     it("handles trivial case") {
-      List.doubleToString(List(0.0)) should be(List("0.0"))
+      doubleToString(List(0.0)) should be(List("0.0"))
     }
   }
 
-  describe(".map") {
+  describe("map") {
     it("works to implement .addOne") {
-      List.map(List(0, 1, 2))(_ + 1) should be(List(1, 2, 3))
+      map(List(0, 1, 2))(_ + 1) should be(List(1, 2, 3))
     }
 
     it("works to implement .doubleToString") {
-      List.map(List(0.0, 10.0))(_.toString) should be(List("0.0", "10.0"))
+      map(List(0.0, 10.0))(_.toString) should be(List("0.0", "10.0"))
     }
   }
 
-  describe(".filter") {
+  describe("filter") {
     it("can remove all odd numbers from a list") {
-      List.filter(List(1, 2, 3, 4))(_ % 2 == 0) should be(List(2, 4))
+      filter(List(1, 2, 3, 4))(_ % 2 == 0) should be(List(2, 4))
     }
 
     it("doesn't change list if filter doesn't match") {
-      List.filter(List(1, 2, 3, 4, 5))(_ => true) should be(List(1, 2, 3, 4, 5))
+      filter(List(1, 2, 3, 4, 5))(_ => true) should be(List(1, 2, 3, 4, 5))
     }
 
     it("works for a stringy function") {
-      List.filter(List("abc", "def", "ghij"))(_.length() == 3) should be(List("abc", "def"))
+      filter(List("abc", "def", "ghij"))(_.length() == 3) should be(List("abc", "def"))
     }
   }
 }
