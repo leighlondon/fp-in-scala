0cb1c111e7f6c9a9aabb5ffdb864b6fa590bd15d
Leigh London <leigh.00.london@gmail.com>
2018-09-10 19:22:02 +1000

    tree sizing

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -3,3 +3,10 @@ package fpinscala.datastructures
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(v)      => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
+}
