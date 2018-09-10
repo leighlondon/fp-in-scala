bd02c71c300dc5dcacd4edd7b44104300209042b
Leigh London <leigh.00.london@gmail.com>
2018-09-10 22:59:02 +1000

    foldin'

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -24,4 +24,9 @@ object Tree {
     case Leaf(v)      => Leaf(f(v))
     case Branch(l, r) => Branch(map(l)(f), map(r)(f))
   }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v)      => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
 }
