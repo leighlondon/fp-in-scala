ef90717f03b32ef4a23541ee605c2a3590cd2211
Leigh London <leigh.00.london@gmail.com>
2018-09-11 20:06:28 +1000

    implementing flatmap and getorelse

--- src/main/scala/fpinscala/errors/Option.scala
+++ src/main/scala/fpinscala/errors/Option.scala
@@ -1,17 +1,21 @@
 package fpinscala.errors
 
-import scala.{Either => _, Option => _, Some => _, _}
+import scala.{Either => _, Option => _, Some => _}
 
 sealed trait Option[+A] {
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
 
   def map[B](f: A => B): Option[B] = this match {
     case None    => None
     case Some(a) => Some(f(a))
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
-
-  def getOrElse[B >: A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None    => default
+    case Some(a) => a
+  }
 
   def orElse[B >: A](ob: => Option[B]): Option[B] = ???
 
