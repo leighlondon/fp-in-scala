7153b20675cd1a1d1510fcd5cbff907c55031e45
Leigh London <leigh.00.london@gmail.com>
2018-09-11 19:54:26 +1000

    map implementation

--- src/main/scala/fpinscala/errors/Option.scala
+++ src/main/scala/fpinscala/errors/Option.scala
@@ -4,7 +4,10 @@ import scala.{Either => _, Option => _, Some => _, _}
 
 sealed trait Option[+A] {
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match {
+    case None    => None
+    case Some(a) => Some(f(a))
+  }
 
   def flatMap[B](f: A => Option[B]): Option[B] = ???
 
