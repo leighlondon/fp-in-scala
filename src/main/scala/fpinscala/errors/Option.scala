8087a73b388036615badd23a8005a04ec5796e06
Leigh London <leigh.00.london@gmail.com>
2018-09-11 20:40:51 +1000

    working on variance

--- src/main/scala/fpinscala/errors/Option.scala
+++ src/main/scala/fpinscala/errors/Option.scala
@@ -27,3 +27,7 @@ sealed trait Option[+A] {
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object Option {
+  def variance(xs: Seq[Double]): Option[Double] = ???
+}
