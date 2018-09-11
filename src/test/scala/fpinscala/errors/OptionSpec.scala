b23d8d50f546e813b02eddabd4febbf6209d8f13
Leigh London <leigh.00.london@gmail.com>
2018-09-11 20:12:58 +1000

    flatmap specs

--- src/test/scala/fpinscala/errors/OptionSpec.scala
+++ src/test/scala/fpinscala/errors/OptionSpec.scala
@@ -7,15 +7,15 @@ import scala.{Option => _, Some => _}
 class OptionSpec extends FunSpec with Matchers {
   describe("map") {
     it("works for the same type") {
-      Some(1) map(_ * 2) shouldBe Some(2)
+      Some(1) map (_ * 2) shouldBe Some(2)
     }
 
     it("works for different types") {
-      Some(1) map(_.toString) shouldBe Some("1")
+      Some(1) map (_.toString) shouldBe Some("1")
     }
 
     it("works for none as well") {
-      None map(_.toString) shouldBe None
+      None map (_.toString) shouldBe None
     }
   }
 
@@ -26,6 +26,20 @@ class OptionSpec extends FunSpec with Matchers {
 
     it("works with none") {
       None getOrElse None shouldBe None
+    }
+  }
+
+  describe("flatmap") {
+    it("works for some type") {
+      Some(100) flatMap ((i: Int) => Some(i * 2)) shouldBe Some(200)
+    }
+
+    it("works with none as well") {
+      Some(100000) flatMap(_ => None) shouldBe None
+    }
+
+    it("goes from none to none in five seconds") {
+      None flatMap(_ => None) shouldBe None
     }
   }
 }
