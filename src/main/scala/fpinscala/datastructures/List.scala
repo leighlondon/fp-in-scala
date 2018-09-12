6132d846c546b6e0b35d45da16812d6d95a4ce15
Leigh London <leigh.00.london@gmail.com>
2018-09-12 21:49:11 +1000

    naming

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -11,7 +11,7 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 object List {
 
   // Exercise 3.1, evaluating a match expression
-  val x_from_match: Int = List(1, 2, 3, 4, 5) match {
+  val x3: Int = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _)))          => x
     case Nil                                   => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
