ea80951dbd197fb684d57da2c68b2a374d656fec
Leigh London <leigh.00.london@gmail.com>
2018-09-21 15:58:52 +1000

    2map2furious

--- src/main/scala/fpinscala/errors/Option.scala
+++ src/main/scala/fpinscala/errors/Option.scala
@@ -36,4 +36,7 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (aa => b.map(bb => f(aa, bb)))
 }
