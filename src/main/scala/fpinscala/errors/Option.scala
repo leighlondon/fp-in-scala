bc93a6903ca65cc2686460544bfd4aba44d40f6b
Leigh London <leigh.00.london@gmail.com>
2018-10-05 23:49:31 +1000

    woops

--- src/main/scala/fpinscala/errors/Option.scala
+++ src/main/scala/fpinscala/errors/Option.scala
@@ -55,6 +55,6 @@ object Option {
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
     a match {
       case Nil    => Some(Nil)
-      case h :: t => map2(f(h), traverse(t)(f))(_ :: _))
+      case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
     }
 }
