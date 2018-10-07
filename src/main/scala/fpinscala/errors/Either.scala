1d54788e872cc4ed1d2a756d2dcf06ebead0c776
Leigh London <leigh.00.london@gmail.com>
2018-10-07 22:25:54 +1100

    for comprehension with map2

--- src/main/scala/fpinscala/errors/Either.scala
+++ src/main/scala/fpinscala/errors/Either.scala
@@ -20,7 +20,9 @@ sealed trait Either[+E, +A] {
       case Right(a) => Right(a)
     }
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {aa <- this; bb <- b} yield f(aa, bb)
+
 }
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
