cdc3f9bdd1fc8cec776655e15e37b215f4b99cd7
Leigh London <leigh.00.london@gmail.com>
2018-10-09 12:55:29 +1100

    adding headoption with explicit thunking

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,6 +1,12 @@
 package fpinscala.laziness
 
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+
+  def headOption: Option[A] = this match {
+    case Empty      => None
+    case Cons(h, _) => Some(h())
+  }
+}
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
